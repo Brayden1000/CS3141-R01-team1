498a6c053755dfec17f40946ac43b9c74c308c98
Nat Anderson <114156696+and-nat-876@users.noreply.github.com>
2022-10-25 16:59:56 -0400

    Updated login.php
    Made it so that login receives the user information from home page, sets session variables, and then goes back to the home page.
    
    Still needs things like checking if its an mtu email or an admin and such, but that'll come later.

--- web_app/app/login.php
+++ web_app/app/login.php
@@ -1,19 +1,46 @@
 <?php
 require "db.php";
 session_start(); //Starts session to store cookies and stuff
+
+// Make sure user info is present, if not send them back to home screen
+if (!(
+    isset($_POST["id"]) &&
+    isset($_POST["name"]) &&
+    isset($_POST["email"]))) {
+                
+    $url = "https://mtuelevatordown.000webhostapp.com/";
+    header("Location: $url");
+    die();
+        
+}
+
+// Set session values
+$_SESSION["id"] = $_POST["id"];
+$_SESSION["name"] = $_POST["name"];
+$_SESSION["email"] = $_POST["email"];
+
+$_SESSION["logged_in"] = 1;
+$url = "https://mtuelevatordown.000webhostapp.com";
+header("Location: $url");
+die();
+
 ?>
 
-<!DOCTYPE html>
 <html>
     <head>
         <title>Login</title>
+        
+        <?php
+        
+        ?>
+        
     </head>
 
     <body>
-        <h1>Elevator Down: Login</h1>
-        <br>
-        <h3>Website in Testing Phase</h3>
-
-        <br><br><br><p>Add Googe Login stuff here</p>
+        
+        <p>
+            Please wait. Logging you in now...
+        </p>
+        
     </body>
 </html>
