f2ae9a86329b5463c1723e4ff9f51fa1222f99d4
Nat Anderson <nathanan@mtu.edu>
2022-11-01 15:38:34 -0400

    Create mobileAPI.php
    Implemented the mobile API. Details on use can be found in masterkey.txt in the main directory of the repository.

---
+++ web_app/app/mobileAPI.php
@@ -0,0 +1,130 @@
+<?php
+require "db.php";
+session_start();
+
+//if (!isset($_POST["data"])) {
+//    header("LOCATION: https://mtuelevatordown.000webhostapp.com");
+//    die();
+//}
+
+if (isset($_REQUEST['fisher'])) {
+    echo "0";
+}
+
+// Login
+if (isset($_REQUEST['login'])) {
+    
+    $email = $_REQUEST['login'];
+    
+    // Handle login stuff
+    $isValid = checkUser($email);
+    
+    if (1 == $isValid) {
+        $result = isUserInDB($email);
+
+        if ($result == 0) {
+            addUser($email);
+        }
+        echo "1";
+    } else {
+        echo "0";
+    }
+    
+}
+
+// Get Elevator Info
+if (isset($_REQUEST['info'])) {
+    
+    $id = $_REQUEST['info'];
+    if (0 == strcmp($id, "all")) {
+        
+        $info = getReportNumber();
+        
+        for ($i = 0; $i < count($info); $i++) {
+            
+            echo $info[$i]['id'];
+            echo $info[$i]['downReports'];
+            echo $info[$i]['location'];
+            
+        }
+        
+    } else {
+        echo "Singular Elevator info not implemented yet. Sorry for the inconvienience.";
+    }
+    
+}
+
+// Is User Admin
+if (isset($_REQUEST['admin'])) {
+    
+    $email = $_REQUEST['admin'];
+    
+    $isAdmin = isUserAdmin($email);
+    
+    echo $isAdmin;
+    
+}
+
+// Make User Report
+if (isset($_REQUEST['report'])) {
+    
+    if (isset($_REQUEST['email']) && isset($_REQUEST['elevatorID'])) {
+        
+        $email = $_REQUEST['email'];
+        $elevID = $_REQUEST['elevatorID'];
+        $comment = isset($_REQUEST['comment']) ? $_REQUEST['comment'] : "";
+        
+        makeUserReport($email, $elevID, $comment);
+        
+        echo "1";
+        
+    } else {
+        echo "0";
+        echo "Missing email or elevatorID";
+    }
+    
+}
+
+// Verify Elevator Down
+if (isset($_REQUEST['verify'])) {
+    
+    if (isset($_REQUEST['elevatorID']) && isset($_REQUEST['email'])) {
+        
+        $elevID = $_REQUEST['elevatorID'];
+        $email = $_REQUEST['email'];
+        
+        if (1 == isUserAdmin($email)) {
+            verifyElevator($elevID, $email);
+            
+            echo "1";
+        } else {
+            echo "-1";
+        }
+        
+    } else {
+        echo "0";
+    }
+    
+}
+
+// Get Reports
+if (isset($_REQUEST['viewReports'])) {
+    
+    $elevID = $_REQUEST['viewReports'];
+    
+    $reports = getReports($elevID);
+    $numReports = count(reports);
+    echo $numReports;
+    
+    for ($i = 0; $i < $numReports; $i++) {
+        
+        echo $reports[$i]['elevatorId'];
+        echo $reports[$i]['id'];
+        echo $reports[$i]['reporter'];
+        echo $reports[$i]['comment'];
+        
+    }
+    
+}
+  
+?>
