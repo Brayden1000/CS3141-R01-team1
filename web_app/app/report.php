b6404e86648217a7745f331df80aa6f4a1e4b812
Nat Anderson <nathanan@mtu.edu>
2022-12-06 14:53:16 -0500

    Update report.php
    Changed textfield to radio buttons
    
    Includes Vincent's changes trying to get map view to use popups

--- web_app/app/report.php
+++ web_app/app/report.php
@@ -20,13 +20,54 @@ if (isset($_POST['process'])) {
 
 <?php
     
-    die();
+    //die();
     
 }
 
-if (isset($_POST['report']) || isset($_GET['report'])) {
-    $report_elevator_id = (isset($_POST['report'])) ? $_POST['report_elevator_id'] : $_GET['report_elevator_id'];
-    $report_elevator_location = (isset($_POST['report'])) ? $_POST['report_elevator_location'] : $_GET['report_elevator_location'];
+if (isset($_SESSION['map'])) {
+    echo "blah";
+    unset($_SESSION['map']);
+    ?>
+    <script>
+        window.opener.location.reload();
+        window.close();
+    </script>
+    <?php
+    die();
+}
+
+if (isset($_GET['report'])) {
+    echo "test";
+    $_SESSION['map'] = 1;
+    ?>
+    <form id = "RedirectGet" onsubmit='target_popup(this)' action="/report.php" method="post">
+    <input type="hidden" name="report_elevator_id" value="<?php echo $_GET['report_elevator_id']; ?>">
+    <input type="hidden" name="report_elevator_location" value="<?php echo str_replace("_", " ", $_GET['report_elevator_location']); ?>">
+    <input type="hidden" name="report" value="Report Elevator">
+    <input type="hidden" name="testing" value="hello">
+    <input type="submit">
+    </form>
+
+    <script type="text/javascript">
+        function target_popup(form) {
+            window.open('https://mtuelevatordown.000webhostapp.com/report.php', 'formpopup', 'width=600,height=600');
+            form.target = 'formpopup';
+        }
+
+        //document.getElementById('RedirectGet').submit(); // SUBMIT FORM
+    </script>
+
+    <?php
+}
+
+if (isset($_POST['report'])) {
+    
+    if (isset($_POST['testing'])) {
+        echo $_POST['testing'];
+    }
+    
+    $report_elevator_id = $_POST['report_elevator_id'];
+    $report_elevator_location = $_POST['report_elevator_location'];
 
 	foreach (getReportNumber() as $elevator) {
 		if ($elevator['id'] == $report_elevator_id) {
@@ -63,8 +104,18 @@ if (isset($_POST['report']) || isset($_GET['report'])) {
             <form action="/report.php" method="post">
                 <input type="hidden" name="process" value="1">
                 <input type="hidden" name="report_id" value="<?php echo $report_elevator_id; ?>">
-                <textarea name="comment" maxlength="255" rows="10" cols="80"></textarea>
+                
+            	<input type="radio" id="stuck" name="comment" value="Elevator stuck at specific floor">
+            	<label for="stuck">Elevator stuck at a specific floor</label><br>
+            
+            	<input type="radio" id="doors" name="comment" value="Elevator doors won't open">
+            	<label for="stuck">Elevator doors won't open</label><br>
+            
+            	<input type="radio" id="call" name="comment" value="Elevator can't be called from certain floors">
+            	<label for="stuck">Elevator can't be called from certain floors</label><br>
+            	
                 <br>
+                
                 <input type="submit" value="Submit" class="form_button">
             </form>
         </div>
